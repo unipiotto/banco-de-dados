4a3615dc47ed2b42d00a6d2f1dfbc143932386f2
LBonardi23 <lbonardi2312@gmail.com>
2024-11-30 16:00:01 -0300

    Criação da estrutura do trabalho

---
+++ ProjetoBancoDeDados/src/test/java/org/example/projetobancodedados/ProjetoBancoDeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projetobancodedados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancoDeDadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
